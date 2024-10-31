5ac734ada11f19203edee9124fddc4fa839855a3
Leandro Abel Marchiori <39203828+leompa@users.noreply.github.com>
2024-10-30 22:54:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("https://script.googleusercontent.com/macros/echo?user_content_key=o0LjKBsyqE5MwRvfrqBnVfY1w4RGarAZLwH-1pGz9EYZeDJR77Naxvy7KcZHK2o0as_WNG6HqFstHFPHtNQfw9vnQ0rIWeIRm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEF23EP3a6bvUAc2vPPYfZ7LY03CD1pq---0zmjsBeDtd3ddD4bWRDAsbjOnc613AGUcCNTItoflZTSBGR11ZKe3m9ySqb-74A&lib=ML4XgOul4Y3Nsn3yzDKxbwq2Qpd7lkeiX") // Reemplaza con tu URL
+      .get("http://xsminternet.com/inverter/test.php") // Reemplaza con tu URL
       .then((response) => {
         setData(response.data);
         setLoading(false);
