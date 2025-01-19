29e94db1ffe0729f908a9e4c9b64340e751bcf6d
Leandro Abel Marchiori <leompa2016@gmail.com>
2025-01-19 19:02:53 -0300

    Conexion a esp32

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 import ReactSpeedometer from "react-d3-speedometer";
-import axios from 'axios';
-
+import axios from "axios";import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
 
 function App() {
   const [data, setData] = useState(null);
@@ -20,30 +21,43 @@ function App() {
         setError(error.message);
         setLoading(false);
       });
-  }, []); 
+  }, []);
 
   if (loading) return <p>Cargando...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-      <ReactSpeedometer value={data.bateria_v} maxValue={30} minValue={20} segmentColors={[
-    "#D62728",
-    "#FFD92F",
-    "#2CA02C",
-    "#2CA02C",
-    "#D62728",
-  ]}/>
-  <ReactSpeedometer value={data.pv_volt} maxValue={36} minValue={20} segments={4} />
-  <ReactSpeedometer value={data.w_salida} maxValue={1500} minValue={0} />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+    <h1>Mpp Solar Info</h1>
+    <Grid container spacing={2} columns={{ xs: 12, sm: 8, md: 12 }}>
+      <Grid item xs={12} md={3}>
+        <Paper style={{ padding: "16px", textAlign: "center" }}>
+          <ReactSpeedometer
+            value={data.bateria_v}
+            maxValue={30}
+            minValue={20}
+            segmentColors={["#D62728", "#FFD92F", "#2CA02C", "#2CA02C", "#D62728"]}
+          />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Paper style={{ padding: "16px", textAlign: "center" }}>
+          <ReactSpeedometer value={data.pv_volt} maxValue={36} minValue={20} segments={4} />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Paper style={{ padding: "16px", textAlign: "center" }}>
+          <ReactSpeedometer value={data.pv_amp} maxValue={30} minValue={0} />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Paper style={{ padding: "16px", textAlign: "center" }}>
+          <ReactSpeedometer value={data.w_salida} maxValue={1500} minValue={0} />
+        </Paper>
+      </Grid>
+    </Grid>
+  </div>
+  );
 }
 
-export default App
+export default App;
